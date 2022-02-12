90c35a5be2622c4e6bc15e174a8e14036dac17d5
Aliaksander <mobilebrainz@gmail.com>
2022-02-12 11:25:13 +0300

    + product Fragment

--- app/src/main/java/app/khodko/catfood/ui/product/ProductFragment.kt
+++ app/src/main/java/app/khodko/catfood/ui/product/ProductFragment.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import app.khodko.catfood.R
+import app.khodko.catfood.api.onliner.ProductResponse
 import app.khodko.catfood.core.BaseFragment
 import app.khodko.catfood.core.extension.getViewModelExt
+import app.khodko.catfood.core.extension.showExt
 import app.khodko.catfood.data.ProductResult
 import app.khodko.catfood.data.SearchResult
 import app.khodko.catfood.databinding.FragmentProductBinding
@@ -18,6 +20,7 @@ class ProductFragment : BaseFragment() {
     private var _binding: FragmentProductBinding? = null
     private val binding get() = _binding!!
     private lateinit var productViewModel: ProductViewModel
+    private lateinit var product: ProductResponse
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,6 +34,7 @@ class ProductFragment : BaseFragment() {
             productViewModel = getViewModelExt { ProductViewModel(key) }
             initObservers()
         }
+        binding.swipeRefreshLayout.isEnabled = false
         return binding.root
     }
 
@@ -38,13 +42,55 @@ class ProductFragment : BaseFragment() {
         productViewModel.product.observe(viewLifecycleOwner) {
             when (it) {
                 is ProductResult.Success -> {
-                    //showErrorSnackbar(R.string.error_app, R.string.snackbar_retry) {  }
+                    product = it.data
+                    with(product) {
+                        binding.nameView.text = fullName
+                        binding.descriptionView.text = description
+                        showPrice(product)
+                        showImage(product)
+                        initListeners(product)
+                    }
                 }
                 is ProductResult.Error -> {
                     showErrorSnackbar(R.string.error_app, R.string.snackbar_retry)
                     { productViewModel.load() }
                 }
             }
+        }
+    }
+
+    private fun showPrice(product: ProductResponse) {
+        product.prices?.let {
+            var priceStr = ""
+            it.priceMin?.let { p -> priceStr += p.amount + " " + p.currency }
+            it.priceMax?.let { p ->
+                if (p.amount != it.priceMin?.amount) {
+                    priceStr += " - " + p.amount + " " + p.currency
+                }
+            }
+            if (priceStr.isNotEmpty()) {
+                it.offers?.count?.let { c ->
+                    val amountStr = getString(R.string.offers)
+                    priceStr += " ($c $amountStr)"
+                }
+                binding.priceView.text = priceStr
+            }
+        }
+    }
+
+    private fun showImage(product: ProductResponse) {
+        val imageUrl = product.images?.header
+        if (imageUrl != null && imageUrl.isNotEmpty()) {
+            binding.imageView.showExt("https://$imageUrl")
+        } else {
+            binding.imageView.setImageResource(R.drawable.ic_image_24)
+        }
+    }
+
+    private fun initListeners(product: ProductResponse) {
+        binding.btnLink.setOnClickListener {
+            val link = product.htmlUrl
+            // todo: web-view
         }
     }
 
